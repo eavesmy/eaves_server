6d403f93825662d54a64eeea4d3c9a31f8a3de1a
Zhang <zdy23216340@outlook.com>
2016-05-24 17:49:24 +0800

    update

--- controller/terminal/index.js
+++ controller/terminal/index.js
@@ -4,12 +4,24 @@ exports.render = function(req, res) {
     res.render('eaves-terminal', {
         _privite: 'editor'
     });
-}
+};
 
 exports.code = function(req, res) {
+    var resSend = function(status,info){
+        (status === 404)?(info = 'Sorry,we dont know what you input..'):(true);
+        var _data = {
+            _status:status,
+            _info:info
+        };
+        res.send(_data);
+    };
+
     var code = req.body._code,
         query = req.body._query;
     codeOrder.doing(code,query,function(result){
-    	console.log(result);
+        if(result === undefined){
+            return resSend(404)
+        }
+        res.send(200,result);
     });
-}
+};
--- webpack-js/terminal.js
+++ webpack-js/terminal.js
@@ -7,65 +7,12 @@ let _info = {
     age:25
 };
 
+
+
 class Terminal extends React.Component{
-    componentDidMount(){
-        window.addEventListener('keypress',function(e){
-            (e.keyCode === 13)?(CodeInterface()):(false)
-        });
-
-        var CodeInterface = ()=>{
-            var domShow = this.refs.userShow,
-                a = this.refs.userInput,
-                div = document.createElement('div');
-            div.setAttribute('class','text-reminder');
-            div.innerText = '>'+a.value;
-            domShow.appendChild(div);
-            CodeDeal(a.value);
-            a.value = '';
-        };
-
-        var CodeDeal = (_code)=>{
-            var code,query;
-            _code = _code.split(' ');
-            code = _code.splice(0,1).toString();
-            query = _code.toString();
-            ajax({
-                url:'/terminal',
-                method:'POST',
-                data:{
-                    _code:code,
-                    _query:query
-                },
-                success:function(res){
-                    CodeShow(res);
-                }
-            });
-        };
-
-        var CodeShow = (main)  =>{
-            var domShow = this.refs.userShow,
-                row = document.createElement('div');
-            row.setAttribute('class','row');
-            (main._status === 404)?(row.innerHTML = '<span class="text-waring">'+main._status+'</span>'+main._info):(row.innerHTML = '<span>'+main._info+'</span>')
-            domShow.appendChild(row);
-        }
-    }
     render(){
         return <div>
-            <div className="title">
-                EAVES
-            </div>
-            <div ref="userShow" className="body">
-                {this.props.name},use <span className="text-reminder">help</span> to show how to use this.
-            </div>
-            <div className="input">
-                <span className="input-title">
-                    <span className="text-unactivated">visitor</span>
-                    <span className="text-logo">@EAVES</span>
-                    $
-                </span>
-                <input ref="userInput" id="user-input" type="text" placeholder="Tap here"/>
-            </div>
+            aaa
         </div>
     }
 }
