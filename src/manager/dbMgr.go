a85434dc4bea21f41d1c7e608dd8a621bb74d7d8
eaves <zdy23216340@163.com>
2017-03-14 18:26:06 +0800

    update

--- src/manager/dbMgr.go
+++ src/manager/dbMgr.go
@@ -1,14 +1,19 @@
 package manager
 
+import(
+	"gopkg.in/redis.v5"
+)
+
 type DBdata struct {
 	Name    string
 	Handler string
+	Data	string
 }
 
 var DBpip []DBdata
 
-func DBinsert(insertInfo string) {
-
+func DBinsert(insertData *DBdata) {
+	DBpip = append(DBpip,insertData)
 }
 
 func DBcheck() {
@@ -16,11 +21,17 @@ func DBcheck() {
 }
 
 func DBloop() {
-	//if len(DBpip) != 0 {
-	//	dealData(DBpip[0,1])
-	//}
+	if len(DBpip) != 0 {
+		dealData(DBpip[0,1])
+	}
 }
 
 func dealData(_data *DBdata) {
+	_handler := _data.Handler
+	
+	switch handler{
+		case "insert":
+			
 
+	}
 }
